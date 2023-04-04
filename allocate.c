7f29d6c076527fd8cb12ca79390096e0bf1b9a6a
Dillon Wong <dillonhanren@student.unimelb.edu.au>
2023-04-05 01:57:05 +1000

    parsing processes from input file

--- allocate.c
+++ allocate.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <assert.h>
+#include <math.h>
 
 // The number of expected command line arguments
-int N_ARGC = 9;
+#define N_ARGC 9
+// The scale by which the `processes` array is dynamically reallocated 
+#define REALLOC_SCALE 2
+// The number of attributes per process
+#define N_ATTRIBUTES 4
+// The maximum length of process names
+#define MAX_NAME_LEN 9
+// The maximum length of a line from an input file
+#define MAX_LINE_LEN 35
+
+// Each process has an arrival time, a unique name, a service time, and a memory requirement
+typedef struct process{
+    int time_arr;
+    char name[MAX_NAME_LEN];
+    int serv_time;
+    int mem_req;
+}process_t;
+
 
 int main(int argc, char* argv[]){
     int opt;
@@ -12,9 +32,12 @@ int main(int argc, char* argv[]){
     char *memory_strategy;
     int quantum;
 
+    size_t n_processes = 0;
+    size_t processes_size = 5;
+
     if (argc != N_ARGC){
         fprintf(stderr, "Error: malformed command line arguments\n");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     while ((opt = getopt(argc, argv, ":f:s:m:q:")) != -1){
@@ -28,7 +51,7 @@ int main(int argc, char* argv[]){
             memory_strategy = optarg;
         }
         else if (opt == 'q'){
-            quantum = (int)optarg[0];
+            quantum = atoi(optarg);
         } 
     }
 
@@ -36,5 +59,40 @@ int main(int argc, char* argv[]){
     printf("scheduler: %s\n", scheduler);
     printf("memory-strategy: %s\n", memory_strategy);
     printf("quantum: %d\n", quantum);
+    
+    process_t *processes = (process_t*)malloc(sizeof(process_t) * processes_size);
+    assert(processes != NULL);
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL){
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE_LEN];
+    int time_arr, serv_time, mem_req;
+    char name[MAX_NAME_LEN];
+
+    while (fgets(line, MAX_LINE_LEN, fp)){
+        sscanf(line, "%d %s %d %d", &time_arr, name, &serv_time, &mem_req);
+        if (n_processes == processes_size){
+            processes_size *= REALLOC_SCALE;
+            processes = (process_t*)realloc(processes, sizeof(process_t) * processes_size);
+            assert(processes);
+        }
+        printf("%zu\n", n_processes);
+        processes[n_processes].time_arr = time_arr;
+        strcpy(processes[n_processes].name, name);
+        processes[n_processes].serv_time = serv_time;
+        processes[n_processes].mem_req = mem_req;
+        n_processes++;
+    }
+
+    for (int i = 0; i < n_processes; i++){
+        printf("%d %s %d %d\n", processes[i].time_arr, processes[i].name, processes[i].serv_time, processes[i].mem_req);
+    } 
+
+    free(processes);
+    fclose(fp);
     return 0;
 }
